acca4d9443c7fd7824af2481b5fbe7d2df657193
Mackaronie <stremmchen@web.de>
2026-06-08 11:59:40 +0200

    Update Lichtsystem_.ino

--- Projekt/Lichtsystem_.ino
+++ Projekt/Lichtsystem_.ino
@@ -1,11 +1,10 @@
-//-------------------------------//
-//WPU Projekt von Aaron und David//
-//-------------------------------//
-
 void Lichtsystem() {
   tasterstatus = digitalRead(Lichtschalter);
 
-  if(tasterstatus == HIGH) {
-    ToogleLight(Licht); //nutzt direkt die Klasse vom HouseLight anstatt selber eine neue Sache zu machen
+  if (tasterstatus == LOW && letzterTasterstatus == HIGH) {
+    ToggleLight(Licht);
+    delay(50);
   }
+  
+  letzterTasterstatus = tasterstatus;
 }
