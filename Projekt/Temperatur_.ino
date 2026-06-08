eaf59ff8f8c092a884b201e12275f4cc54ca77a3
Mackaronie <stremmchen@web.de>
2026-06-08 12:00:44 +0200

    Update Temperatur_.ino

--- Projekt/Temperatur_.ino
+++ Projekt/Temperatur_.ino
@@ -1,9 +1,12 @@
-//-------------------------------//
-//WPU Projekt von Aaron und David//
-//-------------------------------//
+void SetupTemperature(int pin) {
+  thermistor = new NTC_Thermistor(pin, 10000, 10000, 25, 3950);
+}
 
 void Temperatursystem() {
-  RohOuputTemperatur = analogRead(Temperatursensor);
-  temperatur = map(RohOuputTemperatur, 0, 410, -50, 150);
-  delay(TemperaturUpdateDelay);
+  if (thermistor) {
+    temperatur = thermistor->readCelsius();
+  } else {
+    RohOuputTemperatur = analogRead(Temperatursensor);
+    temperatur = map(RohOuputTemperatur, 0, 410, -50, 150);
+  }
 }
