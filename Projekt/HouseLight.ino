081feeabf49d3ee81b1c59c7612b9f4424554f28
Mackaronie <stremmchen@web.de>
2026-06-08 11:59:13 +0200

    Update HouseLight.ino

--- Projekt/HouseLight.ino
+++ Projekt/HouseLight.ino
@@ -1,15 +1,9 @@
-//-------------------------------//
-//WPU Projekt von Aaron und David//
-//-------------------------------//
-
-
-bool on = false;
-
 void SetupHouseLight(int pin) {
   pinMode(pin, OUTPUT);
+  digitalWrite(pin, LOW);
 }
 
-void ToogleLight(int pin) {
-  digitalWrite(pin, !on);
-  on = !on;
+void ToggleLight(int pin) {
+  Lichtstatus = !Lichtstatus;
+  digitalWrite(pin, Lichtstatus ? HIGH : LOW);
 }
